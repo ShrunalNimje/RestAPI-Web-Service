8f373068aad4e9802f31e72dae605962689571aa
Shrunal Nimje <shrunalnimje@gmail.com>
2024-08-04 19:29:21 +0530

    Initial commit

---
+++ src/main/java/my/mood/restAPI/RestAPI/Web/Service/RestApiWebServiceApplication.java
@@ -0,0 +1,13 @@
+package my.mood.restAPI.RestAPI.Web.Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/my/mood/restAPI/RestAPI/Web/Service/RestApiWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package my.mood.restAPI.RestAPI.Web.Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
