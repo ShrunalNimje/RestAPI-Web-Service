c18b651515e30afa2a93898d701dec6e7f58b5ab
Shrunal Nimje <shrunalnimje@gmail.com>
2024-08-06 20:43:52 +0530

    created exception class

---
+++ src/main/java/my/mood/restAPI/RestAPI/Web/Service/user/UserNotFoundException.java
@@ -0,0 +1,13 @@
+package my.mood.restAPI.RestAPI.Web.Service.user;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
+public class UserNotFoundException extends RuntimeException {
+
+	public UserNotFoundException(String message) {
+		super(message);
+	}
+	
+}
