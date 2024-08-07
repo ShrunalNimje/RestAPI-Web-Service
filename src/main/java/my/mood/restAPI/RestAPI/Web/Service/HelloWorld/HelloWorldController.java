0419877d0e07a6438345f2c64cc4316b548529da
Shrunal Nimje <shrunalnimje@gmail.com>
2024-08-07 20:47:50 +0530

    created RestAPI for internationalization

--- src/main/java/my/mood/restAPI/RestAPI/Web/Service/HelloWorld/HelloWorldController.java
+++ src/main/java/my/mood/restAPI/RestAPI/Web/Service/HelloWorld/HelloWorldController.java
@@ -1,5 +1,10 @@
 package my.mood.restAPI.RestAPI.Web.Service.HelloWorld;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 //import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
+	@Autowired
+	private MessageSource messageSource;
+	
+	public HelloWorldController(MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
+	
 //	@RequestMapping(path = "hello-world", method = RequestMethod.GET)
 //	public String helloWorld() {
 //		return "Hello World";
@@ -28,4 +40,12 @@ public class HelloWorldController {
 	public HelloWorldBean helloWorlPathVariable(@PathVariable String name) {
 		return new HelloWorldBean(String.format("Hello World, %s", name));
 	}
+	
+	@GetMapping(path = "hello-world-internationalization")
+	public String helloWorldInternationalization() {
+		
+		Locale locale = LocaleContextHolder.getLocale();
+		return messageSource.getMessage("good.morning.message", null, "default message", locale );
+	}
+	
 }
