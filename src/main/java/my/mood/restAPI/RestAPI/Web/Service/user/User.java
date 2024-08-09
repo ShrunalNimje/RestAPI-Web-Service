94f8a72956412cf452c695c283f34b5a811f671a
Shrunal Nimje <shrunalnimje@gmail.com>
2024-08-09 21:55:30 +0530

    customizing fields name

--- src/main/java/my/mood/restAPI/RestAPI/Web/Service/user/User.java
+++ src/main/java/my/mood/restAPI/RestAPI/Web/Service/user/User.java
@@ -2,17 +2,22 @@ package my.mood.restAPI.RestAPI.Web.Service.user;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 public class User {
 
+	@JsonProperty("user_id")
 	private int id;
 	
 	@Size(min = 2, message = "name should be more than 2 characters")
+	@JsonProperty("user_name")
 	private String name;
 	
 	@Past(message = "birth date must be a past date")
+	@JsonProperty("date_of_birth")
 	private LocalDate birthDate;
 	
 	public User(int id, String name, LocalDate birthDate) {
