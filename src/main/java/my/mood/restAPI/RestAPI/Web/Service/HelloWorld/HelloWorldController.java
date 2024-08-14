ae4007cd16b43b5afe418660936355cc2626a70f
Shrunal Nimje <shrunalnimje@gmail.com>
2024-08-14 20:26:20 +0530

    changes in code

--- src/main/java/my/mood/restAPI/RestAPI/Web/Service/HelloWorld/HelloWorldController.java
+++ src/main/java/my/mood/restAPI/RestAPI/Web/Service/HelloWorld/HelloWorldController.java
@@ -37,7 +37,7 @@ public class HelloWorldController {
 	}
 	
 	@GetMapping(path = "hello-world/path-variable/{name}")
-	public HelloWorldBean helloWorlPathVariable(@PathVariable String name) {
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
 		return new HelloWorldBean(String.format("Hello World, %s", name));
 	}
 	
