e1b9229667ecbab9f4a35526660c5f422bba9f8e
Shrunal Nimje <shrunalnimje@gmail.com>
2024-08-14 20:21:58 +0530

    created data.sql file to h2-database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into User_Details (Id, Birth_Date, Name)
+values (101, Current_Date(), 'Adam');
+
+insert into User_Details (Id, Birth_Date, Name)
+values (102, Current_Date(), 'Shakira');
+
+insert into User_Details (Id, Birth_Date, Name)
+values (103, Current_Date(), 'Shrunal');
+
+insert into User_Details (Id, Birth_Date, Name)
+values (104, Current_Date(), 'Nimje');
