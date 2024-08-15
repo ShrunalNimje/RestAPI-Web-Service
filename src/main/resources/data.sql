7ee7fb536f1da9c9505c6c1b0adf2538a35c1acc
Shrunal Nimje <shrunalnimje@gmail.com>
2024-08-15 22:36:36 +0530

    insert values in post table

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,3 +9,15 @@ values (103, Current_Date(), 'Shrunal');
 
 insert into User_Details (Id, Birth_Date, Name)
 values (104, Current_Date(), 'Nimje');
+
+insert into post (Id, user_id, description)
+values (1001, 102, 'Learn AWS');
+
+insert into post (Id, user_id, description)
+values (1002, 101, 'Learn Cloud');
+
+insert into post (Id, user_id, description)
+values (1003, 101, 'Learn Azure');
+
+insert into post (Id, user_id, description)
+values (1004, 102, 'Learn Kubernetes');
