271ba372c6afc9f8b2ba7990f6fee300a98c5093
Shrunal Nimje <shrunalnimje@gmail.com>
2024-08-15 22:37:24 +0530

    creating object of post

--- src/main/java/my/mood/restAPI/RestAPI/Web/Service/user/User.java
+++ src/main/java/my/mood/restAPI/RestAPI/Web/Service/user/User.java
@@ -1,12 +1,15 @@
 package my.mood.restAPI.RestAPI.Web.Service.user;
 
 import java.time.LocalDate;
+import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 // import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
@@ -29,6 +32,10 @@ public class User {
 	@Past(message = "birth date must be a past date")
 	// @JsonProperty("date_of_birth")
 	private LocalDate birthDate;
+	
+	@OneToMany(mappedBy = "user")
+	@JsonIgnore
+	public List<Post> posts;
 	
 	public User(int id, String name, LocalDate birthDate) {
 		super();
@@ -61,6 +68,14 @@ public class User {
 		this.birthDate = birthDate;
 	}
 
+	public List<Post> getPosts() {
+		return posts;
+	}
+	
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
+	
 	@Override
 	public String toString() {
 		return "User [id=" + id + ", name=" + name + ", birthDate=" + birthDate + "]";
